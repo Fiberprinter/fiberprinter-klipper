7586efe300a0f27641b8e7506c20801e0951f797
Elias Gottsbacher <eli.gottsbacher@gmail.com>
2025-01-30 15:32:51 +0100

    cleanup

--- klippy/extras/fiberprinter/__init__.py
+++ klippy/extras/fiberprinter/__init__.py
@@ -3,10 +3,7 @@ from extras.fiberprinter.fiberextruder import load_extruders
 
 class FiberPrinter:
     def __init__(self, config):
-        self.printer = printer = config.get_printer()
-        self.toolhead = printer.lookup_object('toolhead')
         self.cutter = CutterServo(config.getsection('fiberprinter'))
-        
         load_extruders(config)
         
 def load_config(config):
