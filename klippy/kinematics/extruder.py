397ff6cc0ea153d4ee697a6d275c4e9ac79df1a9
Elias Gottsbacher <eli.gottsbacher@gmail.com>
2025-01-30 17:44:28 +0100

    remove pfusch

--- klippy/kinematics/extruder.py
+++ klippy/kinematics/extruder.py
@@ -304,11 +304,7 @@ class DummyExtruder:
     def get_trapq(self):
         raise self.printer.command_error("Extruder not configured")
 
-import extras.fiberprinter as fiberprinter
-
 def add_printer_objects(config):
-    fiberprinter.add_fiber_printer(config)
-    
     printer = config.get_printer()
     
     for i in range(99):
